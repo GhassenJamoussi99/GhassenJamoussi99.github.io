ebd6e116784c4b6616e6464de8d4832f41c9de65
Ghassen Jamoussi <ghassen.jamoussi99@gmail.com>
2022-03-12 12:03:52 +0100

    Upload web project

---
+++ README.md
@@ -0,0 +1,11 @@
+﻿# Web_Development_Project
+ 
+## Done by:
+     Ghassen Jamoussi
+
+## Academic year:
+     2022 5th Semester
+ 
+## Usage:
+     Clone the project and open index.html
+     To check out the website you can go to https://ghassenjamoussi99.github.io/
---
+++ js/main.js
@@ -0,0 +1,129 @@
+(function () {
+
+  console.log("Width", document.body.offsetWidth)
+  console.log("Height", document.body.offsetHeight)
+
+  // Animation when scrolling
+  AOS.init({
+    duration: 1000,
+    once: true,
+    useClassNames: true,
+    initClassName: false,
+    animatedClassName: 'animated',
+  });
+
+
+  /************ Url: https://github.com/mattboldt/typed.js  ***************/
+  // Selector function
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  // Home type effect
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+  /**************************************************************************/
+
+})()
+
+//https://stackoverflow.com/questions/13840516/how-to-find-my-distance-to-a-known-location-in-javascript
+function distance(lon1, lat1, lon2, lat2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = (lat2 - lat1).toRad();  // Javascript functions in radians
+  var dLon = (lon2 - lon1).toRad();
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+/** Converts numeric degrees to radians */
+if (typeof (Number.prototype.toRad) === "undefined") {
+  Number.prototype.toRad = function () {
+    return this * Math.PI / 180;
+  }
+};
+
+function setup_map() {
+  // Setting a map
+  if ('geolocation' in navigator) {
+    console.log('geolocation available');
+
+    navigator.geolocation.getCurrentPosition(position => {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      acc = position.coords.accuracy;
+
+      document.getElementById('latitude').textContent = lat;
+      document.getElementById('longitude').textContent = lon;
+      document.getElementById('accuracy').textContent = acc;
+
+      dist = distance(lon, lat, 13.35183, 52.54561);
+      document.getElementById('distance').textContent = dist;
+
+      const mymap = L.map('mymap').setView([lat, lon], 15); //set the coordinates in the map
+      const attribution =
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+      const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+      const tiles = L.tileLayer(tileUrl, { attribution });
+      tiles.addTo(mymap);
+      const marker = L.marker([lat, lon]).addTo(mymap);
+    });
+  } else {
+    console.log('geolocation not available');
+  }
+}
+
+//From w3schools https://www.w3schools.com/w3css/w3css_slideshow.asp
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("Slides-v2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) { myIndex = 1 }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+//From w3schools https://www.w3schools.com/w3css/w3css_slideshow.asp
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+}
